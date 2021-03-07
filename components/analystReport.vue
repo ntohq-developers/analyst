b943d73d57e3749e4a4d830c6c02cc1f13fd5326
Wesley Ford <mangofordw@gmail.com>
2021-03-06 18:51:17 -0600

    Carousel Update

--- components/analystReport.vue
+++ components/analystReport.vue
@@ -8,6 +8,9 @@
       <b-tab-item label="ML" icon="">
         <h1 class="title is-6"><u>Machine Learning Stats</u></h1>
       </b-tab-item>
+      <b-tab-item label="News" icon="">
+        <slot />
+      </b-tab-item>
       <b-tab-item label="Settings" icon="gear">
         <h1 class="title is-6"><u>Settings</u></h1>
       </b-tab-item>
--- pages/index.vue
+++ pages/index.vue
@@ -1,133 +1,140 @@
 <template>
   <section class="section">
-    <div class="tile is-ancestor">
-      <div class="tile is-parent is-vertical">
-        <b-field class="tile is-child" style="max-height:50px;" label="Ticker">
-          <b-input
-            v-model="tickerInput"
-            type="text"
-            style="max-width: 200px;"
-            @input="debounceInput"
-          ></b-input>
-        </b-field>
-        <ul class="tile is-child">
-          <li
-            v-for="ticker in searchResults"
-            :key="ticker.symbol"
-            @click="
-              tickerInput = ticker.symbol
-              companyName = ticker.name
-              fetchSearchData()
-            "
+    <div>
+      <div class="tile is-ancestor">
+        <div class="tile is-parent is-vertical">
+          <b-field
+            class="tile is-child"
+            style="max-height:50px;"
+            label="Ticker"
           >
-            <b-tooltip :label="ticker.name" position="is-right" animated>
-              {{ ticker.symbol }}
-            </b-tooltip>
-          </li>
-        </ul>
-      </div>
-      <div style="margin-bottom: 15px; flex: 3;">
-        <TestChart
-          :cc-type="chartTypes[currentStyle]"
-          :chart-name="chartLabel"
-          :tiker-name="tickerInput"
-          :company-name="companyName"
-          :labels="labels"
-          :input-data="data"
-        ></TestChart>
-        <div>
-          <b-dropdown
-            v-model="currentStyle"
-            :scrollable="true"
-            aria-role="list"
-            mobile-modal="true"
-          >
-            <button slot="trigger" class="button is-primary" type="button">
-              <template>
-                <span>{{ currentStyle }}</span>
-              </template>
-              <b-icon icon="menu-down"></b-icon>
-            </button>
-            <b-dropdown-item
-              v-for="(value, name) in chartTypes"
-              :key="name"
-              :value="name"
-              aria-role="listitem"
+            <b-input
+              v-model="tickerInput"
+              type="text"
+              style="max-width: 200px;"
+              @input="debounceInput"
+            ></b-input>
+          </b-field>
+          <ul class="tile is-child">
+            <li
+              v-for="ticker in searchResults"
+              :key="ticker.symbol"
+              @click="
+                tickerInput = ticker.symbol
+                companyName = ticker.name
+                fetchSearchData()
+              "
             >
-              <span>{{ name }}</span>
-            </b-dropdown-item>
-          </b-dropdown>
+              <b-tooltip :label="ticker.name" position="is-right" animated>
+                {{ ticker.symbol }}
+              </b-tooltip>
+            </li>
+          </ul>
         </div>
-      </div>
-      <div class="tile is-parent">
-        <div class="tile is-child is-vertical">
-          <b-button
-            style="margin-bottom: 2em;"
-            type="is-primary"
-            @click="quickLink"
-            >Quick Link</b-button
-          >
-          <b-field label="Period">
-            <b-select v-model="period">
-              <option value="max">Max</option>
-              <option value="10y">Ten years</option>
-              <option value="5y">Five years</option>
-              <option value="2y">Two years</option>
-              <option value="1y">One year</option>
-              <option value="6mo">Six months</option>
-              <option value="3mo">Three months</option>
-              <option value="1mo">One month</option>
-              <option value="1wk">One week</option>
-              <option value="5d">Five days</option>
-              <option value="1d">One day</option>
-            </b-select>
-          </b-field>
-          <b-field label="Interval">
-            <b-select v-model="interval">
-              <option value="3mo">Three months</option>
-              <option value="1mo">One month</option>
-              <option value="1wk">One week</option>
-              <option value="5d">Five days</option>
-              <option value="1d">One day</option>
-              <option value="1h">One hour</option>
-              <option value="30m">Thirty minutes</option>
-              <option value="15m">Fifteen minutes</option>
-              <option value="5m">Five minutes</option>
-              <option value="2m">Two minutes</option>
-              <option value="1m">One minutes</option>
-            </b-select>
-          </b-field>
-          <b-button style="margin-top: 1em;" @click="fetchStockData"
-            >Fetch Data</b-button
-          >
-        </div>
-      </div>
-      <div class="section cards">
-        <b-button
-          style="margin-top: 2em; margin-bottom: 2em;"
-          @click="fetchNewsData"
-          >Fetch News</b-button
-        >
-        <div v-for="article in articles" :key="article.title" class="card">
-          <div class="card-image">
-            <figure class="image">
-              <img :src="article.urlImage" />
-            </figure>
+        <div style="margin-bottom: 15px; flex: 3;">
+          <TestChart
+            :cc-type="chartTypes[currentStyle]"
+            :chart-name="chartLabel"
+            :tiker-name="tickerInput"
+            :company-name="companyName"
+            :labels="labels"
+            :input-data="data"
+          ></TestChart>
+          <div>
+            <b-dropdown
+              v-model="currentStyle"
+              :scrollable="true"
+              aria-role="list"
+              mobile-modal="true"
+            >
+              <button slot="trigger" class="button is-primary" type="button">
+                <template>
+                  <span>{{ currentStyle }}</span>
+                </template>
+                <b-icon icon="menu-down"></b-icon>
+              </button>
+              <b-dropdown-item
+                v-for="(value, name) in chartTypes"
+                :key="name"
+                :value="name"
+                aria-role="listitem"
+              >
+                <span>{{ name }}</span>
+              </b-dropdown-item>
+            </b-dropdown>
           </div>
-          <div class="card-content">
-            <div class="media">
-              <div class="media-content">
-                <p class="title is-4">{{ article.title }}</p>
+        </div>
+        <div class="tile is-parent">
+          <div class="tile is-child is-vertical">
+            <b-button
+              style="margin-bottom: 2em;"
+              type="is-primary"
+              @click="quickLink"
+              >Quick Link</b-button
+            >
+            <b-field label="Period">
+              <b-select v-model="period">
+                <option value="max">Max</option>
+                <option value="10y">Ten years</option>
+                <option value="5y">Five years</option>
+                <option value="2y">Two years</option>
+                <option value="1y">One year</option>
+                <option value="6mo">Six months</option>
+                <option value="3mo">Three months</option>
+                <option value="1mo">One month</option>
+                <option value="1wk">One week</option>
+                <option value="5d">Five days</option>
+                <option value="1d">One day</option>
+              </b-select>
+            </b-field>
+            <b-field label="Interval">
+              <b-select v-model="interval">
+                <option value="3mo">Three months</option>
+                <option value="1mo">One month</option>
+                <option value="1wk">One week</option>
+                <option value="5d">Five days</option>
+                <option value="1d">One day</option>
+                <option value="1h">One hour</option>
+                <option value="30m">Thirty minutes</option>
+                <option value="15m">Fifteen minutes</option>
+                <option value="5m">Five minutes</option>
+                <option value="2m">Two minutes</option>
+                <option value="1m">One minutes</option>
+              </b-select>
+            </b-field>
+            <b-button style="margin-top: 1em;" @click="fetchStockData"
+              >Fetch Data</b-button
+            >
+          </div>
+        </div>
+      </div>
+      <report>
+        <aside class="section cards">
+          <b-button
+            style="margin-top: 2em; margin-bottom: 2em;"
+            @click="fetchNewsData"
+            >Fetch News</b-button
+          >
+          <div v-for="article in articles" :key="article.title" class="card">
+            <div class="card-image">
+              <figure class="image">
+                <img :src="article.urlImage" />
+              </figure>
+            </div>
+            <div class="card-content">
+              <div class="media">
+                <div class="media-content">
+                  <p class="title is-4">{{ article.title }}</p>
+                </div>
               </div>
             </div>
+            <div class="card-footer">
+              <a :href="article.url" class="card-footer-item">Open article</a>
+            </div>
           </div>
-          <div class="card-footer">
-            <a :href="article.url" class="card-footer-item">Open article</a>
-          </div>
-        </div>
-      </div>
+        </aside>
+      </report>
     </div>
-    <report></report>
   </section>
 </template>
 

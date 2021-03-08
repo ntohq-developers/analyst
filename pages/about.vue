ebe4cf2b19df1bef36521cd1b3d6310b80f9eba0
Wesley Ford <mangofordw@gmail.com>
2021-03-08 09:28:52 -0600

    Update about.vue
    Added content to about page
        - API citations
        - Quick description
        - Credit to Creators

--- pages/about.vue
+++ pages/about.vue
@@ -1,5 +1,60 @@
 <template>
   <section class="box" style="padding: 1.5vh; flex: 1;">
     <h1 class="title is-3">About Analyst</h1>
+    <p class="has-text-black">
+      Analyst is a website that analyzes stocks using Charts, Computational
+      data, AI, and news. Analyst is to be used as a reference not as
+      professional trading advice. Although Analyst is currently Open Source, It
+      should not be copied or redistributed.
+    </p>
+    <section style="margin-top: 20px;">
+      <h1 class="title is-3">The Creators</h1>
+      <div style="display: flex; flex-direction: column;">
+        <div style="margin-bottom: 50px;" class="box">
+          <div>
+            <img
+              src="https://avatars.githubusercontent.com/u/11604664?s=460&u=6e747319287b4a7c22916314db3526ab89e181b9&v=4"
+              style="border-radius: 50%; height: 100px;"
+            />
+          </div>
+          <p>
+            Just an average programmer🧙‍♂️. Web Development, Python 3, C#/C++, and
+            Java
+          </p>
+        </div>
+        <div class="box">
+          <div>
+            <img
+              src="https://avatars.githubusercontent.com/u/20588081?s=400&u=917bfc4e1fbd964293059459b13e080dcaece51e&v=4"
+              style="border-radius: 50%; height: 100px;"
+            />
+          </div>
+          <p>
+            A pseudonormal programmer. Web Development 🌐, UI/UX 👨‍🎨, and C/C++
+            💻!
+          </p>
+        </div>
+      </div>
+    </section>
+    <section style="margin-top: 30px; padding: 20px;">
+      <h1 class="title is-3 has-text-centered">Powered by:</h1>
+      <div style="display: flex; flex-direction: column;">
+        <cite
+          >yfinance:
+          <a href="https://github.com/ranaroussi/yfinance"
+            >https://github.com/ranaroussi/yfinance</a
+          ></cite
+        >
+        <cite
+          >TickerSymbol:
+          <a href="https://github.com/yashwanth2804/TickerSymbol"
+            >https://github.com/yashwanth2804/TickerSymbol</a
+          ></cite
+        >
+        <cite
+          >News Api: <a href="https://newsapi.org">https://newsapi.org</a></cite
+        >
+      </div>
+    </section>
   </section>
 </template>

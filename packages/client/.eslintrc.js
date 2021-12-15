c2d17f99a06d1d22cb8b0ac00aedfd8aaf38e67d
Wesley Ford <mangofordw@gmail.com>
2021-12-14 23:22:39 -0600

    Migrated to a Monorepo format

--- packages/client/.eslintrc.js
+++ packages/client/.eslintrc.js
@@ -19,5 +19,8 @@ module.exports = {
   ],
   // add your custom rules here
   rules: {
+   'vue/multi-word-component-names': 'off',
+   'vue/no-template-key': 'off',
+   'vue/component-tags-order': 'off'
   }
 }

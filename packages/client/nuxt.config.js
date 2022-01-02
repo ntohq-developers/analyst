af01f9ab565defa653d4b913ce28a9bc9abe6f47
Wesley Ford <mangofordw@gmail.com>
2022-01-01 21:26:18 -0600

    Upgrade nuxt2 to nuxt3 beta

--- packages/client/nuxt.config.js
+++ packages/client/nuxt.config.js
@@ -1,94 +1,37 @@
-export default {
-  mode: 'spa',
-  /*
-   ** Headers of the page
-   */
-  head: {
-    title: process.env.APP_NAME || 'analyst',
-    meta: [
-      { charset: 'utf-8' },
-      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      {
-        hid: 'description',
-        name: 'description',
-        content: process.env.npm_package_description || ''
-      }
-    ],
-    link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }]
-  },
-  /*
-   ** Customize the progress-bar color
-   */
-  loading: { color: '#fff' },
-  /*
-   ** Global CSS
-   */
-  css: [],
-  /*
-   ** Plugins to load before mounting the App
-   */
-  plugins: [{ src: '~/plugins/vue-apexchart.js', ssr: false }],
-  /*
-   ** Nuxt.js dev-modules
-   */
-  buildModules: [
-    // Doc: https://github.com/nuxt-community/eslint-module
-    '@nuxtjs/eslint-module',
-    '@nuxtjs/fontawesome'
-  ],
-  /*
-   ** Nuxt.js modules
-   */
-  modules: [
-    // Doc: https://buefy.github.io/#/documentation
-    ['nuxt-buefy', { css: true, materialDesignIcons: true, iconfont: 'mdi' }],
-    // Doc: https://axios.nuxtjs.org/usage
-    '@nuxtjs/axios',
-    // Doc: https://github.com/nuxt-community/dotenv-module
-    '@nuxtjs/dotenv',
-    '@nuxtjs/proxy'
-  ],
-  buefy: {
-    defaultIconPack: 'fas',
-    defaultIconComponent: 'FontAwesomeIcon',
-    materialDesignIcons: false
-  },
-  fontawesome: {
-    icons: {
-      // solid: true,  // include the full pack in the bundle, not recommended
-      solid: [
-        'faHome',
-        'faUserCog',
-        'faProjectDiagram',
-        'faNewspaper',
-        'faChartPie',
-        'faCaretDown',
-        'faCaretUp',
-        'faSlidersH',
-        'faAngleRight',
-        'faLink',
-        'faInfoCircle',
-        'faExclamationTriangle',
-        'faHandshake',
-        'faPalette'
-      ] // include only icons required by your project
-    }
-  },
-  /*
-   ** Axios module configuration
-   ** See https://axios.nuxtjs.org/options
-   */
-  axios: {
-    baseURL: process.env.URL || process.env.baseURL
-  },
-  /*
-   ** Build configuration
-   */
-  build: {
-    /*
-     ** You can extend webpack config here
-     */
-    vendor: ['vue-apexchart'],
-    extend(config, ctx) {}
-  }
-}
+import { defineNuxtConfig } from 'nuxt3'
+import sass from 'sass'
+import buefy from 'nuxt-buefy'
+import dotenv from '@nuxtjs/dotenv'
+
+// https://v3.nuxtjs.org/docs/directory-structure/nuxt.config
+export default defineNuxtConfig({
+   head: {
+      title: process.env.APP_NAME || 'analyst',
+      meta: [
+         { charset: 'utf-8' },
+         { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+         {
+            hid: 'description',
+            name: 'description',
+            content: ''
+         }
+      ],
+      link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }]
+   },
+
+   modules: [
+      buefy,
+      dotenv
+   ],
+
+   build: {
+      loaders: {
+         sass: {
+            implementation: sass,
+         },
+         scss: {
+            implementation: sass,
+         },
+      },
+   }
+})
--- packages/server/app.js
+++ packages/server/app.js
@@ -1,30 +1,107 @@
-const express = require("express")
-const stocks  = require("./stock.js")
-const app = express()
+const express         = require("express")
+const axios           = require("axios")
+const { MongoClient } = require('mongodb');
+const app             = express()
+
 require("dotenv").config()
 
-const port = process.env.PORT || 8080
+const MONGO_URL         = `mongodb+srv://${process.env.MONGO_USER}:${process.env.MONGO_PASSWORD}@main.u7pjf.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`
+const mongoClient       = new MongoClient(MONGO_URL);
+const STOCK_APIKEY      = process.env.POLYGON_APIKEY
+const STOCK_REQUEST_URL = "https://api.polygon.io/"
+const STOCK_NEWS_SEARCH = "v2/reference/news?"
+const STATUS            = "v1/marketstatus/now?"
+
+const PORT = process.env.PORT || 8080
+
+const buildRequest = ({ url, ...params }) => {
+   return Object.keys(params).reduce(
+      (accumulator, currentParam) =>
+         `${accumulator}${currentParam}=${params[currentParam]}&`,
+      url
+   )
+}
+
+app.use(express.json())
 
 app.get('/', (req, res) => {
-   res.sendStatus(403)
+   res.sendStatus(403).send({ "message": "PAGE NOT FOUND" })
 })
 
-app.get("/stockQuery/", (req, res) => {
-   const tiker    = req.query.tiker
-   const period   = req.query.period
-   const interval = req.query.interval
-   let   stock    = new stocks()
+app.get('/ai/quote/:ticker', async (req, res) => {
+   const { ticker } = req.params
 
-   if(tiker != undefined) {
-      stock.getData(tiker, period, interval)
-      res.sendStatus(200)
-   } 
-   else {
-      res.sendStatus(400)
-   } 
+   await mongoClient.connect()
+   const db = mongoClient.db.
+   res.sendStatus(503)
+})
+
+app.post('/ai/train/', (req, res) => {
+
+})
+
+app.get('/status/', (req, res) => {
+   let requestUrl = buildRequest({
+      "url": STOCK_REQUEST_URL + STATUS,
+      "apiKey": STOCK_APIKEY
+   })
+   try{
+      axios.get(requestUrl).then((response) => {
+         res.status(200).send({
+            "status": 200,
+            "data": response.data.exchanges
+         })
+      })
+   } catch(er) {
+      console.log(er)
+      res.statu(503).send({
+         "status": "503",
+         "message": "FAILED TO FETCH DATA FROM API; (app.js) ln:x##"
+      })
+   }
 })
 
 
-app.listen(port, () => {
-   console.log(`Runnning on http://localhost:${port}`)
+app.get('/news/:ticker', (req, res) => {
+   const { ticker } = req.params
+
+   let requestUrl = buildRequest({
+      "url": STOCK_REQUEST_URL + STOCK_NEWS_SEARCH,
+      "ticker": ticker,
+      "order": "asc",
+      "limit": 20,
+      "sort": "published_utc",
+      "apiKey": STOCK_APIKEY
+   })
+   
+   try {
+      axios.get(requestUrl).then((response) => {
+         return response.data.results.map((news) => {
+            return {
+               "title":  news.title,
+               "url":    news.article_url,
+               "image":  news.image_url,
+               "getter": news.description
+            }
+         })
+      }).then((data) => {
+         res.status(200).send({
+            "status": 200,
+            "data":   data
+         })
+      })
+   } catch (er) {
+      console.log(er)
+      res.status(503).send({
+         "status": 503,
+         "message": "FAILED TO FETCH DATA FROM API; (app.js) ln:x##"
+      })
+   }
+})
+
+
+
+
+app.listen(PORT, () => {
+   console.log(`Runnning on http://localhost:${PORT}`)
 })
